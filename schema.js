2d0293ffc64301e56698dc1e3bc2a2e8eb6efc46
Peter Czibik <p.czibik@gmail.com>
2019-02-24 17:17:11 +0100

    add mutation

--- schema.js
+++ schema.js
@@ -55,6 +55,15 @@ const User = new GraphQLObjectType({
   }
 });
 
+const UserInputType = new GraphQLInputObjectType({
+  name: "UserInputType",
+  fields: {
+    firstName: { type: new GraphQLNonNull(GraphQLString) },
+    lastName: { type: new GraphQLNonNull(GraphQLString) },
+    email: { type: new GraphQLNonNull(GraphQLString) }
+  }
+});
+
 const queryType = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
@@ -66,6 +75,9 @@ const queryType = new GraphQLObjectType({
       resolve: (_, { filter }) => {
         console.log(filter); // For demonstration purposes
         return users.filter((user) => {
+          if (!filter) {
+            return true;
+          }
           const lefthand = user[filter.field];
           switch (filter.operation) {
             case "eq": {
@@ -84,9 +96,26 @@ const queryType = new GraphQLObjectType({
   }
 });
 
+const mutationType = new GraphQLObjectType({
+  name: "RootMutationType",
+  fields: {
+    createUser: {
+      type: User,
+      args: {
+        input: { type: UserInputType }
+      },
+      resolve(_, args) {
+        users.push(args.input);
+        return args.input;
+      }
+    }
+  }
+});
+
 const schema = new GraphQLSchema({
   types: [User],
-  query: queryType
+  query: queryType,
+  mutation: mutationType
 });
 
 module.exports = schema;
